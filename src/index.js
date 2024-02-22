8252aed4665f77b649513f9b37c35fde11b791cf
@All-that-code <amaia.arbizu.mail@gmail.com>
2024-02-22 21:51:48 +0100

    Modified styling and improved UX

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generateRecommendation(event) {
     "You are a literature expert and avid reader, great at recommending books based on their genre or theme. Please provide three book recommendations in a simple way, providing the titles and their authors. Please change the titles every time and avoid repeating yourself, even if the ${userInput.value} remains the same. Use this answer as a format example: <strong>On the Road</strong>, by Jack Kerouac<br/><strong>Blue Highways</strong>, by William Least Heat-Moon<br/><strong>Travels with Charley in Search of America</strong>, by John Steinbeck";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let resultElement = document.querySelector("#book-results");
+  resultElement.classList.remove("book-teaser");
+  resultElement.innerHTML = `<div class="generating-recommendations">Generating book recommendations on ${userInput.value}</div>`;
+
   axios.get(apiUrl).then(showRecommendations);
 }
 
